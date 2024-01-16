77e0cb2a062621a6ddb611a133006017b202367d
mywur <tvampharos@gmail.com>
2024-01-16 22:51:48 +0100

    First addition (literally) (cleaned)

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,10 +4,8 @@ import src.main.calculations.basicOperations;
 
 public class Main {
 
-
 public static void main(String[] args){
     System.out.println(basicOperations.addition(1, Math.PI));
 }
-
 
 }
